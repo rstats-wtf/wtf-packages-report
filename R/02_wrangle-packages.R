8e1c643536680fa827f63fc5369f89d0413042e7
Shannon Pileggi <pileggis@mskcc.org>
2023-09-10 18:06:46 -0400

    tweaks

--- R/02_wrangle-packages.R
+++ R/02_wrangle-packages.R
@@ -1,30 +1,32 @@
-## remember to restart R here!
+# Restart R here!
 
-## create a data frame by reading from data/installed-packages.csv
-## hint: readr::read_csv() or read.csv()
-## idea: try using here::here() to create the file path
+# 1. Create a data frame by reading from data/installed-packages.csv. ----------
+# hint: readr::read_csv() or read.csv()
+# idea: try using here::here() to create the file path
 
-## filter out the base and recommended packages
-## keep the variables Package and Built
-## if you use dplyr, code like this will work:
-apt <- ipt %>%
-  filter(is.na(Priority)) %>%
+
+# 2. Filter out the base and recommended packages. -----------------------------
+# (retaining the packages added on after installation)
+# Keep the variables Package and Built.
+# If you use dplyr, code like this will work.
+df_pkgs_addon <- df_pkgs |>
+  filter(is.na(Priority)) |>
   select(Package, Built)
 
-## write this new, smaller data frame to data/add-on-packages.csv
-## hint: readr::write_csv() or write.table()
-## idea: try using here::here() to create the file path
+# 3. Write this new, smaller data frame to data/add-on-packages.csv. -----------
+# hint: readr::write_csv() or write.table()
+# idea: try using here::here() to create the file path
 
-## make a frequency table of package by the version in Built
-## if you use dplyr, code like this will work:
-apt_freqtable <- apt %>%
-  count(Built) %>%
+# 4. Summarize the packages with a frequency table of the version in Built. ----
+# if you use dplyr, code like this will work:
+pkgs_addon_freqtable <- df_pkgs_addon |>
+  count(Built) |>
   mutate(prop = n / sum(n))
 
-## write this data frame to data/add-on-packages-freqtable.csv
-## hint: readr::write_csv() or write.table()
-## idea: try using here::here() to create the file path
-
-## YES overwrite the files that are there now
-## they are old output from me (Jenny)
-## they are just examples
+# 5. Write this data frame to data/add-on-packages-freqtable.csv. --------------
+# hint: readr::write_csv() or write.table()
+# idea: try using here::here() to create the file path
+# ----
+# YES overwrite the files that are there now
+# they are old output from Jenny
+# they are just examples
--- R/03_barchart-packages-built.R
+++ R/03_barchart-packages-built.R
@@ -1,18 +1,17 @@
-## remember to restart R here!
+# Restart R here!
 
-## make a barchart from the frequency table in data/add-on-packages-freqtable.csv
+# 1. Read data/add-on-packages-freqtable.csv into a data frame.-----------------
+# hint: readr::read_csv() or read.csv()
+# idea: try using here::here() to create the file path
 
-## read that csv into a data frame
-## hint: readr::read_csv() or read.csv()
-## idea: try using here::here() to create the file path
-
-## if you use ggplot2, code like this will work:
-ggplot(apt_freqtable, aes(x = Built, y = n)) +
+# 2. Make a barchart from the frequency table you read in. ---------------------
+# if you use ggplot2, code like this will work:
+ggplot(pkgs_addon_freqtable, aes(x = Built, y = n)) +
   geom_col()
 
-## write this barchart to figs/built-barchart.png
-## if you use ggplot2, ggsave() will help
-## idea: try using here::here() to create the file path
-
-## YES overwrite the file that is there now
-## that's old output from me (Jenny)
+# 3. Write this barchart to figs/built-barchart.png. ---------------------------
+# f you use ggplot2, ggsave() will help.
+# idea: try using here::here() to create the file path
+# ----
+# YES overwrite the file that is there now
+# that is old output from Jenny
